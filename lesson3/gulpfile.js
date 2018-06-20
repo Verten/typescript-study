9c2318120342b6af718c4a2bb14204de5bbf741d
ebinhon <bin.hong@ericsson.com>
2018-06-20 15:27:57 +0800

    commit lesson3 for typescript with browserify

--- lesson3/gulpfile.js
+++ lesson3/gulpfile.js
@@ -2,25 +2,31 @@ var gulp = require('gulp')
 var browserify = require('browserify')
 var source = require('vinyl-source-stream')
 var tsify = require('tsify')
+var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps')
 var buffer = require('vinyl-buffer')
+var watchify = require('watchify')
+var gutil = require('gulp-util')
 var paths = {
   pages: ['src/*.html'],
 }
 
-gulp.task('copy-html', function() {
-  return gulp.src(paths.pages).pipe(gulp.dest('dist'))
-})
-
-gulp.task('default', ['copy-html'], function() {
-  return browserify({
+var watchedBrowserify = watchify(
+  browserify({
     basedir: '',
     debug: true,
     entries: ['src/index.ts'],
     cache: {},
     packageCache: {},
-  })
-    .plugin(tsify)
+  }).plugin(tsify),
+)
+
+gulp.task('copy-html', function() {
+  return gulp.src(paths.pages).pipe(gulp.dest('dist'))
+})
+
+function bundle() {
+  return watchedBrowserify
     .transform('babelify', {
       presets: ['env'],
       extensions: ['.ts'],
@@ -29,6 +35,11 @@ gulp.task('default', ['copy-html'], function() {
     .pipe(source('bundle.js'))
     .pipe(buffer())
     .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(uglify())
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('dist'))
-})
+}
+
+gulp.task('default', ['copy-html'], bundle)
+watchedBrowserify.on('update', bundle)
+watchedBrowserify.on('log', gutil.log)
